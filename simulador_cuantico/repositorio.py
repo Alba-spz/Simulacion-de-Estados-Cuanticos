9b0ea6cb43ec5f353a326813610aea3176fbb0b8
Alba-spz <albasanchezpozo@gmail.com>
2025-05-13 10:27:01 +0200

    añado mas codigo

--- simulador_cuantico/repositorio.py
+++ simulador_cuantico/repositorio.py
@@ -1,5 +1,5 @@
 from simulador_cuantico.estado import EstadoCuantico
-
+import json
 class RepositorioDeEstados:
     def __init__(self):
         self.estados = {}
@@ -20,8 +20,42 @@ class RepositorioDeEstados:
         except Exception as e:
             print(f"Error al crear el estado cuántico: {e}")
 
+    def guardar(self, archivo: str):
+        lista_estados = []
+        for estado in self.estados.values():
+            lista_estados.append({
+                "id": estado.id,
+                "base": estado.base,
+                "vector": estado.vector
+            })
+        try:
+            with open(archivo, 'w') as file:
+                json.dump(lista_estados, file, indent=4)
+            print(f"Estados guardados en {archivo} ({len(lista_estados)} estados).")
+        except Exception as e:
+            print(f"Error al guardar los estados: {e}")
+
     def obtener_estado(self, id: str):
         return self.estados.get(id, None)
+
+    def cargar(self, archivo: str):
+        try:
+            with open(archivo, 'r') as file:
+                lista_estados = json.load(file)
+
+            self.estados.clear()  # Limpiar estados anteriores
+
+            for estado_data in lista_estados:
+                id = estado_data["id"]
+                base = estado_data["base"]
+                vector = estado_data["vector"]
+                self.agregar_estado(id, vector, base)
+
+            print(f"{len(lista_estados)} estados cargados desde {archivo}.")
+        except FileNotFoundError:
+            print(f"Archivo '{archivo}' no encontrado.")
+        except Exception as e:
+            print(f"Error al cargar los estados: {e}")
 
     def eliminar_estado(self, id: str):
         if id in self.estados:
